cc802a10b2f6ef1dd103e18d6225ec9934b103e2
Nelson Gomes <nelsongomes@MacBook-Pro-de-Nelson.local>
2024-07-18 14:39:15 +0100

    add zoom out to all options: fix 2

--- ucrop/src/main/java/com/yalantis/ucrop/UCropActivity.java
+++ ucrop/src/main/java/com/yalantis/ucrop/UCropActivity.java
@@ -473,8 +473,8 @@ public class UCropActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     mGestureCropImageView.zoomOutImage(mGestureCropImageView.getMinScale());
-                    mGestureCropImageView.setTargetAspectRatio(
-                            ((AspectRatioTextView) ((ViewGroup) v).getChildAt(0)).getAspectRatio(v.isSelected()));
+                    // mGestureCropImageView.setTargetAspectRatio(
+                    //         ((AspectRatioTextView) ((ViewGroup) v).getChildAt(0)).getAspectRatio(v.isSelected()));
                     mGestureCropImageView.setImageToWrapCropBounds();
                     if (!v.isSelected()) {
                         for (ViewGroup cropAspectRatioView : mCropAspectRatioViews) {
