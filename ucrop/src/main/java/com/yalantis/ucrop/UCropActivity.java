f3ea7a09e0eadc04cc65ccd08870c138b61b8ed2
Nelson Gomes <nelsongomes@MacBook-Pro-de-Nelson.local>
2024-07-18 11:45:15 +0100

    remove zoom out

--- ucrop/src/main/java/com/yalantis/ucrop/UCropActivity.java
+++ ucrop/src/main/java/com/yalantis/ucrop/UCropActivity.java
@@ -442,7 +442,7 @@ public class UCropActivity extends AppCompatActivity {
 
             aspectRatioList = new ArrayList<>();
             aspectRatioList.add(new AspectRatio(null, 1, 1));
-            aspectRatioList.add(new AspectRatio(null, 3, 4));
+            aspectRatioList.add(new AspectRatio(null, 3, 10));
             aspectRatioList.add(new AspectRatio(getString(R.string.ucrop_label_original).toUpperCase(),
                     CropImageView.SOURCE_IMAGE_ASPECT_RATIO, CropImageView.SOURCE_IMAGE_ASPECT_RATIO));
             // aspectRatioList.add(new AspectRatio(null, 3, 2));
@@ -474,7 +474,7 @@ public class UCropActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     mGestureCropImageView.setTargetAspectRatio(
                             ((AspectRatioTextView) ((ViewGroup) v).getChildAt(0)).getAspectRatio(v.isSelected()));
-                    mGestureCropImageView.zoomOutImage(mGestureCropImageView.getCurrentScale())
+                    // mGestureCropImageView.zoomOutImage(mGestureCropImageView.getCurrentScale())
                     mGestureCropImageView.setImageToWrapCropBounds();
                     if (!v.isSelected()) {
                         for (ViewGroup cropAspectRatioView : mCropAspectRatioViews) {
