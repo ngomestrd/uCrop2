a46b7efe01cb057de5f9811ec9c7679683d367bc
Nelson Gomes <nelsongomes@MacBook-Pro-de-Nelson.local>
2024-07-19 10:38:45 +0100

    revert isSelect handling

--- ucrop/src/main/java/com/yalantis/ucrop/UCropActivity.java
+++ ucrop/src/main/java/com/yalantis/ucrop/UCropActivity.java
@@ -512,8 +512,10 @@ public class UCropActivity extends AppCompatActivity {
                     mGestureCropImageView.setTargetAspectRatio(
                             ((AspectRatioTextView) ((ViewGroup) v).getChildAt(0)).getAspectRatio(v.isSelected()));
                     mGestureCropImageView.setImageToWrapCropBounds();
-                    for (ViewGroup cropAspectRatioView : mCropAspectRatioViews) {
-                        cropAspectRatioView.setSelected(cropAspectRatioView == v);
+                    if (!v.isSelected()) {
+                        for (ViewGroup cropAspectRatioView : mCropAspectRatioViews) {
+                            cropAspectRatioView.setSelected(cropAspectRatioView == v);
+                        }
                     }
                 }
             });
